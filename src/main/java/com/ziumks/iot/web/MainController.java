14ce85a4e69156344366ddd4887f1c9e3e38701e
haheesu <hhs@ziumks.com>
2020-12-08 16:50:58 +0900

    "??"

--- src/main/java/com/ziumks/iot/web/MainController.java
+++ src/main/java/com/ziumks/iot/web/MainController.java
@@ -1,6 +1,7 @@
 package com.ziumks.iot.web;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -8,8 +9,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.ziumks.iot.domain.Account;
 import com.ziumks.iot.repository.AccountRepository;
@@ -30,8 +33,21 @@ public class MainController {
 		model.addAttribute("accountList", accountList);
 		return "index";
 	}
-	@RequestMapping("/search")
-	public String search(@RequestParam(value="keyword")String keyword, Model model) {
-		List<Account> accountList = repository.findOne(id);
+//	@RequestMapping("/search")
+//	public String search(@RequestParam(value="keyword")String keyword, Model model) {
+//		List<Account> accountList = repository.findOne(id);
+//	}
+	@RequestMapping("/index/{id}")
+	@ResponseBody
+	public Account read(@PathVariable("id")Long id, Model model) {
+		Optional<Account> user = repository.findById(id);
+		Account account = null;
+		if(user.isPresent()) {
+			account = user.get();
+		} else {
+			logger.info("account is null");
+		}
+		logger.info(account);
+		return account;
 	}
 }
