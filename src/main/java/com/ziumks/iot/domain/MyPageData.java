a56523a084fea8fa1b80c3ee32df7841f6d8e04e
haheesu <hhs@ziumks.com>
2020-12-10 17:08:50 +0900

    "MyPageData 추가"

---
+++ src/main/java/com/ziumks/iot/domain/MyPageData.java
@@ -0,0 +1,30 @@
+package com.ziumks.iot.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyPageData {
+
+	long recordsTotal = 0;
+	long recordsFiltered = 0;
+    List data = new ArrayList();
+	public long getRecordsTotal() {
+		return recordsTotal;
+	}
+	public void setRecordsTotal(long recordsTotal) {
+		this.recordsTotal = recordsTotal;
+	}
+	public long getRecordsFiltered() {
+		return recordsFiltered;
+	}
+	public void setRecordsFiltered(long recordsFiltered) {
+		this.recordsFiltered = recordsFiltered;
+	}
+	public List getData() {
+		return data;
+	}
+	public void setData(List data) {
+		this.data = data;
+	}
+    
+}
