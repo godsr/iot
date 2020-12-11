d71b54d0376fb8269aefc6752420c7b88e94cb95
haheesu <hhs@ziumks.com>
2020-12-11 14:56:52 +0900

    "AuthorController, user 업데이트"

---
+++ src/main/java/com/ziumks/iot/web/AuthorController.java
@@ -0,0 +1,57 @@
+package com.ziumks.iot.web;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.ziumks.iot.domain.Author;
+import com.ziumks.iot.repository.AuthorRepository;
+
+@Controller
+@RequestMapping("user")
+public class AuthorController {
+
+	 private static final Logger logger = LogManager.getLogger(MainController.class);
+
+	    @Autowired
+	    AuthorRepository repository;
+
+	    @RequestMapping("/list")
+	    public String author(Model model) {
+	    	List<Author> authorList = repository.findByIdRange(20L);
+	        model.addAttribute("list", authorList);
+	        return "user";
+	    }
+	    
+	    @RequestMapping("/{id}")
+	    @ResponseBody
+	    public Author oneauthor(@PathVariable("id")Long id, Model model) {
+	    	Optional<Author> author = repository.findById(id);
+	    	Author list = null;
+	    	if (author.isPresent()) {
+				list = author.get();
+			} else {
+				logger.info("list is null.");
+			}
+	    	logger.info(list);
+	        return list;
+	    }
+	    
+	    @ResponseBody
+		@RequestMapping(value = "/save", method = RequestMethod.POST)
+		public String updateAuthor(Author author) {
+			System.out.println(author);
+
+			return "index";
+		}
+
+}
