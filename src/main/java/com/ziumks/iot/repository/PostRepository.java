d905c3e48a7ecd2da2c5939d370eba1ea1efbc46
godsr <ryong3008@gmail.com>
2020-12-15 18:04:38 +0900

    ysr

--- src/main/java/com/ziumks/iot/repository/PostRepository.java
+++ src/main/java/com/ziumks/iot/repository/PostRepository.java
@@ -2,6 +2,9 @@ package com.ziumks.iot.repository;
 
 import com.ziumks.iot.domain.Account;
 import com.ziumks.iot.domain.Post;
+
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -10,4 +13,5 @@ import java.util.List;
 public interface PostRepository extends JpaRepository<Post, Long>{
 
 	List<Post> findByTitle(String title);
+
 }
